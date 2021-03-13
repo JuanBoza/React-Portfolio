f313aa4e7d60cafbd408d79910dc7457542892fb
Juan Boza <Juanboza80@gmail.com>
2021-03-13 14:48:43 -0700

    components added to repo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+import Header from "./components/header/navbar";
+import Goals from "./pages/Goals";
+import Checkin from "./pages/Checkin";
+import Progress from "./pages/Progress";
+import Notes from "./pages/Notes";
+
+
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/goals" component={Goals}></Route>
+          <Route exact path="/checkin" component={Checkin}></Route>
+          <Route exact path="/progress" component={Progress}></Route>
+          <Route exact path="/goals/:id" component={Notes}></Route>
+        </Switch>
+      
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
